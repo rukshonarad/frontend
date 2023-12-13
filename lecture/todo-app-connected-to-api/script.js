e2737858918bf037ce36934aaa18c379ba976d28
Rukshona Rad <rukshona.rad@gmail.com>
2023-12-13 15:11:19 -0500

    Add todo app to api

--- lecture/todo-app-connected-to-api/script.js
+++ lecture/todo-app-connected-to-api/script.js
@@ -1,4 +1,20 @@
 class TodoApi {
+    async getOne() {
+        try {
+            const response = await fetch("http://localhost:3000/tasks", {
+                method: "POST",
+                headers: {
+                    " Content-Type": "application/js"
+                }
+            });
+            if (!response.ok) {
+                throw new Error(response.error);
+            }
+            return await response.json();
+        } catch (error) {
+            console.log(error);
+        }
+    }
     async getAll() {
         try {
             const response = await fetch("http://localhost:3000/tasks");
